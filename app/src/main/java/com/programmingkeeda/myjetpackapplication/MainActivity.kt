c938ff4c4ffca5bd59bfa85dd8b9554ea6f659c3
Saurabh Kharade <saurabhkharade2@gmail.com>
2025-07-29 23:53:41 +0530

    Add Row and Column example

--- app/src/main/java/com/programmingkeeda/myjetpackapplication/MainActivity.kt
+++ app/src/main/java/com/programmingkeeda/myjetpackapplication/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.programmingkeeda.myjetpackapplication
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.programmingkeeda.myjetpackapplication.ui.theme.MyJetpackApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,8 +30,7 @@ class MainActivity : ComponentActivity() {
             MyJetpackApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Programming Keeda",
-                        modifier = Modifier.padding(innerPadding)
+                        name = "Programming Keeda", modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
@@ -32,11 +40,34 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello Programming Keeda $name!",
-        modifier = modifier
-    )
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(35.dp),
+        verticalArrangement = Arrangement.Top,
+        Alignment.CenterHorizontally
+    ) {
+        Text("Hello", fontSize = 25.sp)
+        Text("Good Morning Programming Keeda!")
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            Alignment.Top
+        ) {
+            Text("Hello", fontSize = 25.sp)
+            Text("Good Morning", fontSize = 25.sp)
+            Text("Good Morning", fontSize = 25.sp)
+        }
+
+    }
+
+
 }
+
 
 @Preview(showBackground = true)
 @Composable
